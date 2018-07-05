99cb09f2646984afc05694bee3f2560048e737a2
adleonis <gallet.stephane@gmail.com>
2018-07-05 09:35:20 +0200

    1.0

--- contracts/Sentiment.sol
+++ contracts/Sentiment.sol
@@ -1,48 +1,104 @@
-function recordbid(string _message, string _author) public payable{
-        acceptBid(msg.value);  //determine if bid is acceptable
+pragma solidity ^0.4.20;
+// Sentiment is an open messaging/expression Dapp -- 
+// express yourself by publishing a message, your "author name"(can be anything)
+// Each bid is put into a queue, higher bids up front
+// Every 15s, the message at the top of the queue is broadcast, and removed from the queue
+// Express yourself free of sensorship, prices set by the demand for the service
+
+contract Sentiment {
+    
+    struct Bid {
+        string message;
+        string author;
+        address addres;
+        uint256 paid;
+        uint256 tim;
+    }
+    
+    address public superuser;
+    uint256 public cBal;
+    
+    constructor() public{
+        //Create initial Bid
+        Bid storage initialBid = Bids[msg.sender];
+        initialBid.message = "Hello World! Express yourself!";
+        initialBid.author = "Stephane Gallet";
+        initialBid.addres = msg.sender;
+        initialBid.paid = 20;
+        initialBid.tim = now;
+        bidListStor.push(initialBid);
+        superuser = msg.sender; //set superuser as the contract deployer
+    }
+    
+    mapping(address => Bid) public Bids;
+    Bid[] public bidListStor;
+    uint256 public minAmount = 20;
+    
+    event broadcastNewBid(
+        string message,
+        string author,
+        address addres,
+        uint256 paid,
+        uint256 tim
+    );
+    
+    function recordbid(string _message, string _author) public payable{
+        Bid[] storage bidList = bidListStor; // copy over bidList from storage to manipulate
+        acceptBid(msg.value, _message);  //determine if bid is acceptable
         
         // Create Bid
-        Bid memory currentBid = Bids[msg.sender];
+        Bid storage currentBid = Bids[msg.sender];
         currentBid.message = _message;
         currentBid.author = _author;
+        currentBid.addres = msg.sender;
         currentBid.paid = msg.value;
-        authorAcct.push(msg.sender); //Save Bid author address in authorAcct list
-        
+        currentBid.tim = now;
         //Figure out the position to insert new bid
-        uint256 position = bidList.length-1;
+        uint256 position = bidList.length;
+        uint256 vpaid = bidList[position-1].paid; //max paid
         
-        /*
-        for (uint i=bidList.length; i>0; i--){
-            uint256 paid = bidList[i-1].paid;
-            if (paid < msg.value)
-                position = i-1;
-                break;
-        } */
-        uint256 paid = bidList[position].paid;
-        while(paid > msg.value){
-            position--;
+        while (vpaid > msg.value && position > 0){
+            position --;
+            vpaid = bidList[position-1].paid;
         }
         
-        // Push all higher bids down by one
-        for (uint j=bidList.length; j>position; j--){
-            bidList[j] = bidList[j-1];
+        if (position < bidList.length){
+            // Push all higher bids down by one
+            bidList.push(bidList[bidList.length-1]); //augment list size by 1, copy last item
+            
+            for (uint j=bidList.length-1; j>=position; j--){
+                bidList[j] = bidList[j-1];
+            } 
+            bidList[position] = currentBid; // save new bid in correct position
+        } else {
+            bidList.push(currentBid); // save new bid in correct position
         }
-        bidList[position] = currentBid; // save new bid in correct position
-        
+        bidListStor = bidList;  //copy manipulated list onto public list
+        emit broadcastNewBid(currentBid.message, currentBid.author,currentBid.addres,currentBid.paid,currentBid.tim);
     }
     
-    function displayAuthors() view public returns(address[]){
-        return authorAcct;
-    }
-    
-    function acceptBid(uint256 received) public payable returns(bool accept){
-        uint minAmount = 20;
-        require (received >= minAmount);
+    function acceptBid(uint256 received, string _message) public payable returns(bool accept){
+        require (received >= minAmount); //check amount paid is sufficient to get on list
+        require (bytes(_message).length <= 10); //check max message size is not exceeded
         return true;
     }
-    /*
-    function sortListInsert(uint256 paid){
-        //Insert Sorting Algorithm
-        //uint256 compare = 
-    }*/
+    
+    function resetMin(uint256 newMin) public{
+        //Allow superuser to reset minimum bid amount
+        require(msg.sender == superuser);
+        minAmount = newMin;
+    }
+    
+    function updateBal() public {
+        require(msg.sender == superuser);
+        cBal = address(this).balance;
+    }
+    
+    function withDraw() public{
+        require(msg.sender == superuser);
+        msg.sender.transfer(address(this).balance);
+        updateBal();
+    }
+    
+    //TODO write fallback function, accept ether
 }
---
+++ controller.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+''' 
+CONTROLLER
+**************************************************************************
+Main site Controller.  Uses flask to throw templates, 
+
+
+'''
+
+from flask import Flask, jsonify, render_template, request, redirect, url_for, flash
+from flask_login import LoginManager, login_required, current_user
+from flask_bootstrap import Bootstrap
+import flask_login
+import os
+
+
+#import model
+#import view
+import datetime
+import time
+#import orm
+#import scraper
+
+login_manager = LoginManager()
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+Bootstrap(app)
+
+@app.template_filter('BMk')
+def translate_followers(s):
+    if s > 999999999:
+        return str(round(s/1000000000,1))+'B'
+    elif s > 999999:
+        return str(round(s/1000000,1))+'M'
+    elif s > 999:
+        return str(round(s/1000,1))+'k'
+    else:
+        return s
+
+@app.template_filter('ctime')
+def timectime(s):
+    return time.ctime(float(s)) # datetime.datetime.fromtimestamp(s)
+
+
+@app.context_processor
+def override_url_for():
+    ''' 
+    This code is by Eric Buckley from http://flask.pocoo.org/snippets/40/
+    It updates your static files everytime the browser refreshes so styles are up to date, not busted by browser cache
+    '''
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    ''' 
+    This code is by Eric Buckley from http://flask.pocoo.org/snippets/40/
+    It updates your static files everytime the browser refreshes so styles are up to date, not busted by browser cache
+    '''
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def show_app():
+    ''' 
+    ..Main website page -- form to input bid
+    '''
+    if request.method == 'GET':
+        # This is an HTTP get request.
+        return render_template('index.html')
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=True)
+
